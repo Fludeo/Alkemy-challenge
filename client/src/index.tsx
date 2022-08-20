b5a27479f318dcee786f3eef8b186ff7cd371161
Leandro Medina <medinaleandron@gmail.com>
2022-08-20 01:47:02 -0300

    final fixes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   
-    <BrowserRouter> 
+    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}> 
             <App />
     </BrowserRouter>
   
